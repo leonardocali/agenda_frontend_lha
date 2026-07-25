ac8146cb234421f0f16d63b530ac456795e82f88
leonardocali <alejaleojulian@gmail.com>
2026-07-25 15:12:01 -0500

    Modify base URL

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const baseUrl = "https://api-agenda-lha.onrender.com/api/persons";
-const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "https://api-agenda-lha.onrender.com/api/persons";
+//const baseUrl = "http://localhost:3001/api/persons";
 
 const getAll = () =>{
     const request = axios.get(baseUrl)
